457e348a42092202cad645d4131270987ead94c0
alex800121 <alex800121@hotmail.com>
2022-08-25 10:31:48 +0800

    First flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Home Manager Flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { nixpkgs, home-manager, ... }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations.alex800121 = home-manager.lib.homeMangerConfiguration {
+      inherit pkgs;
+      modules = [
+        ./home.nix
+      ];
+    };
+  };
+}
